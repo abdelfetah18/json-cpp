1c7b17fb1b79f3be99109989d52d5323b2107dd0
abdelfetah18 <nstart0319@gmail.com>
2023-02-06 21:55:59 +0100

    small changes/fixes

--- CMakeFiles/JsonCpp.dir/data_structures/GenericParser.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/JsonCpp.dir/data_structures/GenericParser.cpp.o: \
- /home/abdelfetah-dev/Desktop/JsonCpp/data_structures/GenericParser.cpp \
- /usr/include/stdc-predef.h \
- /home/abdelfetah-dev/Desktop/JsonCpp/data_structures/GenericParser.h \
- /home/abdelfetah-dev/Desktop/JsonCpp/data_structures/StringView.h
--- CMakeFiles/JsonCpp.dir/data_structures/StringView.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/JsonCpp.dir/data_structures/StringView.cpp.o: \
- /home/abdelfetah-dev/Desktop/JsonCpp/data_structures/StringView.cpp \
- /usr/include/stdc-predef.h \
- /home/abdelfetah-dev/Desktop/JsonCpp/data_structures/StringView.h
